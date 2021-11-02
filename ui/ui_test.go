2a199786319085d72415311c51dedd61b64a932d
Karel Kubat <karel@kubat.nl>
2021-11-02 23:27:52 +0100

    shouldSchedule tests added

--- ui/ui_test.go
+++ ui/ui_test.go
@@ -1,8 +1,11 @@
 package ui
 
 import (
+	"goto-meet/cache"
+	"goto-meet/item"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -28,6 +31,48 @@ func TestNew(t *testing.T) {
 			t.Errorf("New(for notifier %q) == _,%v, want nil error", test.notifier, err)
 		case err != nil && test.wantError != "" && !strings.Contains(err.Error(), test.wantError):
 			t.Errorf("New(for notifier %q) == _,%v, want error with %q", test.notifier, err, test.wantError)
+		}
+	}
+}
+
+func TestShouldSchedule(t *testing.T) {
+	for _, test := range []struct {
+		title       string
+		startsIn    time.Duration
+		joinLink    string
+		wantOutcome bool
+	}{
+		{
+			title:       "pass",
+			startsIn:    time.Hour,
+			joinLink:    "whatever",
+			wantOutcome: true,
+		},
+		{
+			title:       "in the past",
+			startsIn:    -1 * time.Hour,
+			joinLink:    "whatever",
+			wantOutcome: false,
+		},
+		{
+			title:       "no join link",
+			startsIn:    time.Hour,
+			wantOutcome: false,
+		},
+	} {
+		n := &Notifier{
+			opts: &Opts{
+				StartsIn: time.Minute * 30, // consider anything that starts half an hour
+			},
+			processed: cache.New(),
+		}
+		it := &item.Item{
+			Title:    "whatever",
+			JoinLink: test.joinLink,
+			StartsIn: test.startsIn,
+		}
+		if outcome, _ := n.shouldSchedule(it); outcome != test.wantOutcome {
+			t.Errorf("%v: shouldSchedule(%v) = %v, want %v", test.title, it, outcome, test.wantOutcome)
 		}
 	}
 }
