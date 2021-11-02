30d94e2985e905b76f23d7b77c2b1202d2b6ea17
Karel Kubat <karel@kubat.nl>
2021-11-02 22:04:59 +0100

    cache abstracted

---
+++ cache/cache.go
@@ -0,0 +1,49 @@
+// Package cache implements a k/v store of items.
+package cache
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"goto-meet/item"
+)
+
+// Cache is the receiver that wraps necessary data.
+type Cache struct {
+	m map[string]*item.Item
+}
+
+// New returns an initialized cache.
+func New() *Cache {
+	return &Cache{
+		m: map[string]*item.Item{},
+	}
+}
+
+// Lookup returns true when an item was previously stored. When not, the item is stored and false is returned.
+func (c *Cache) Lookup(it *item.Item) bool {
+	k := itemKey(it)
+	if _, ok := c.m[k]; ok {
+		return true
+	}
+	log.Printf("notification added to cache: %v", it)
+	c.m[k] = it
+	return false
+}
+
+// Weed removes items with timestamps in the past. These don't have to be kept in memory.
+func (c *Cache) Weed() {
+	now := time.Now()
+	for k, it := range c.m {
+		if it.Start.Before(now) {
+			log.Printf("notification removed from cache: %v (it's in the past)", it)
+			delete(c.m, k)
+		}
+	}
+}
+
+// itemKey is a helper to derive a distinctive key for an item.
+func itemKey(it *item.Item) string {
+	return fmt.Sprintf("%v::%v::%v::%v", it.Title, it.JoinLink, it.CalendarLink, it.Start)
+}
---
+++ cache/cache_test.go
@@ -0,0 +1,138 @@
+package cache
+
+import (
+	"fmt"
+	"goto-meet/item"
+	"testing"
+	"time"
+)
+
+func TestItemKey(t *testing.T) {
+	now := time.Now()
+	for _, test := range []struct {
+		title        string
+		joinLink     string
+		calendarLink string
+		start        time.Time
+		wantKey      string
+	}{
+		{
+			title:        "title",
+			joinLink:     "joinLink",
+			calendarLink: "calendarLink",
+			start:        now,
+			wantKey:      fmt.Sprintf("title::joinLink::calendarLink::%v", now.String()),
+		},
+	} {
+		it := &item.Item{
+			Title:        test.title,
+			JoinLink:     test.joinLink,
+			CalendarLink: test.calendarLink,
+			Start:        now,
+		}
+		key := itemKey(it)
+		if key != test.wantKey {
+			t.Errorf("itemKey(%v) = %v, want %v", it, key, test.wantKey)
+		}
+	}
+}
+
+func TestLookup(t *testing.T) {
+	now := time.Now()
+	c := New()
+	items := []struct {
+		title        string
+		joinLink     string
+		calendarLink string
+	}{
+		{
+			title:        "1",
+			joinLink:     "2",
+			calendarLink: "3",
+		},
+		{
+			title:        "4",
+			joinLink:     "5",
+			calendarLink: "6",
+		},
+	}
+
+	// Insert items, they must be added
+	for _, test := range items {
+		it := &item.Item{
+			Title:        test.title,
+			JoinLink:     test.joinLink,
+			CalendarLink: test.calendarLink,
+			Start:        now,
+		}
+		if c.Lookup(it) {
+			t.Errorf("Lookup(%v) = true", it)
+		}
+	}
+	// Do it again, they must already be present
+	for _, test := range items {
+		it := &item.Item{
+			Title:        test.title,
+			JoinLink:     test.joinLink,
+			CalendarLink: test.calendarLink,
+			Start:        now,
+		}
+		if !c.Lookup(it) {
+			t.Errorf("Lookup(%v) = false", it)
+		}
+	}
+}
+
+func TestWeed(t *testing.T) {
+	before := time.Now().Add(time.Hour * -1)
+	after := time.Now().Add(time.Hour)
+	c := New()
+
+	items := []struct {
+		title        string
+		joinLink     string
+		calendarLink string
+		start        time.Time
+		wantFound    bool // want this to be found after weeding?
+	}{
+		{
+			title:        "1",
+			joinLink:     "2",
+			calendarLink: "3",
+			start:        after,
+			wantFound:    true,
+		},
+		{
+			title:        "4",
+			joinLink:     "5",
+			calendarLink: "6",
+			start:        before,
+			wantFound:    false,
+		},
+	}
+
+	// Add items, all must be flagged as "added"
+	for _, test := range items {
+		it := &item.Item{
+			Title:        test.title,
+			JoinLink:     test.joinLink,
+			CalendarLink: test.calendarLink,
+			Start:        test.start,
+		}
+		if c.Lookup(it) {
+			t.Errorf("Lookup(%v) = false", it)
+		}
+	}
+	c.Weed()
+	for _, test := range items {
+		it := &item.Item{
+			Title:        test.title,
+			JoinLink:     test.joinLink,
+			CalendarLink: test.calendarLink,
+			Start:        test.start,
+		}
+		if found := c.Lookup(it); found != test.wantFound {
+			t.Errorf("Lookup(%v) = %v, want %v", it, found, test.wantFound)
+		}
+	}
+}
